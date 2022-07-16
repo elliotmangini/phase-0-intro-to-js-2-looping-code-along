88c8c28a91f0ff947b78dc0a5ec5bb618abc015b
Elliot Mangini <elliot.mangini@gmail.com>
2022-07-16 18:29:49 -0400

    commit tree b9473465001e895146b35bb0622542d299dd4de9

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions, homie.
+
+function writeCards (anArray) {
+    let thankYouArray = [];
+
+    for (let i = 0; i < anArray.length; i++) {
+        thankYouArray.push(`Thank you, ${anArray[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thankYouArray;
+}
+
+
+function countDown(aNum) {
+    while (aNum >= 0) {
+        console.log(aNum);
+        aNum--;
+    }
+}
